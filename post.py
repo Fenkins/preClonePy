4b807f431c0e01bcd4c5dbee301f406396ac9131
Anatoly Kovalev <anatoly_kovalev@dbi-718-16x.rnd.dbi>
2018-02-13 04:22:46 +0300

    Time zones issue appears to be fixed

--- post.py
+++ post.py
@@ -2,26 +2,34 @@ import pysnow
 import shelve
 import os.path
 
-import time
-time.gmtime()
-current_time = time.localtime()
-utc = '{}{:0>2}{:0>2}'.format('-' if time.altzone > 0 else '+', abs(time.altzone) // 3600, abs(time.altzone // 60) % 60)
-print(utc)
-print(type(utc))
-print(int(utc))
-print(int('-0500'))
 
-offset =int()
-# Setting the timeModifier
-if utc!='-0500':
-	# Need to calculate time offset
-	print 'Triggered 1'
-	offset = -int(utc) + int('-0500')
-	print('Offset is:')
-	print(offset)
-else:
+import pytz
+
+from datetime import datetime
+
+
+#Donot need that crap either
+#import time
+#time.gmtime()
+#current_time = time.localtime()
+#utc = '{}{:0>2}{:0>2}'.format('-' if time.altzone > 0 else '+', abs(time.altzone) // 3600, abs(time.altzone // 60) % 60)
+#print(utc)
+#print(type(utc))
+#print(int(utc))
+#print(int('-0500'))
+
+#It looks like we are not going to need it
+#offset =int()
+## Setting the timeModifier
+#if utc!='-0500':
+#	# Need to calculate time offset
+#	print 'Triggered 1'
+#	offset = -int(utc) + int('-0500')
+#	print('Offset is:')
+#	print(offset)
+##else:
 	# No need to do shit
-	print 'Triggered 2'
+#	print 'Triggered 2'
 
 osPath = os.path.abspath(__file__)
 filePath = osPath.rsplit('/',1)[0] + '/accountInfo'
@@ -76,23 +84,49 @@ print responseCHGDict['end_date']
 # Response received
 print(type(responseCHGDict['end_date']))
 print(str(responseCHGDict['end_date']))
-print'integer'
 
 
-# Calculating proper start/end time 
-datePartStart = str(responseCHGDict['start_date'])[:11]
-print(datePartStart)
-# Received date in format of 2018-06-01
-timePartStart = str(responseCHGDict['start_date'])[11:]
-print(timePartStart)
-# Received time in format of 14:00:00
-datePartEnd = str(responseCHGDict['end_date'])[:11]
-timePartEnd = str(responseCHGDict['end_date'])[:11]
+
+
+
+est = pytz.timezone('America/New_York')
+utc = pytz.utc
+# 2018-07-01 14:00:00
+# fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+fmt = '%Y-%m-%d %H:%M:%S'
+
+#winter = datetime(2016, 1, 24, 18, 0, 0, tzinfo=utc)
+#summer = datetime(2016, 7, 24, 18, 0, 0, tzinfo=utc)
+
+#print winter.strftime(fmt)
+#print summer.strftime(fmt)
+
+#print winter.astimezone(est).strftime(fmt)
+#print summer.astimezone(est).strftime(fmt)
+
+# 2018-07-01 14:00:00
+
+startTimeEDT = datetime.strptime(responseCHGDict['start_date'], '%Y-%m-%d %H:%M:%S')
+endTimeEDT = datetime.strptime(responseCHGDict['end_date'], '%Y-%m-%d %H:%M:%S')
+
+startTimeEDT = startTimeEDT.replace(tzinfo=utc)
+endTimeEDT = endTimeEDT.replace(tzinfo=utc)
+print '--------------'
+print'Finals'
+
+#Don't need this one
+#print startTimeDT.strftime(fmt)
+
+#print startTimeEDT.astimezone(est).strftime(fmt)
+
+startTimeEDTs = str(startTimeEDT.astimezone(est).strftime(fmt))
+endTimeEDTs = str(endTimeEDT.astimezone(est).strftime(fmt))
+
 
 # Set the payload
 new_record = {
     'short_description': 'Pre-clone checklist for ' + chgNumber,
-    'description': 'Please perform pre-clone checklist for '+ chgNumber + '\nChange Request start time: ' + responseCHGDict['start_date'] + '\nChange Request end time: ' + responseCHGDict['end_date'],
+    'description': 'Please perform pre-clone checklist for '+ chgNumber + '\nChange Request start time: ' + startTimeEDTs + '\nChange Request end time: ' + endTimeEDTs,
     'company': 'Data Intensity',
     'task_for': 'ankovalev',
     'cmdb_ci': 'zzData Intensity - Infrastructure',
