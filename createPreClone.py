77b6d6c82f0f64d77f1f2345ece9c43dd936761e
Anatoly Kovalev <anatoly_kovalev@dbi-718-16x.rnd.dbi>
2018-02-13 05:26:32 +0300

    All clear and looking good now

--- createPreClone.py
+++ createPreClone.py
@@ -1,35 +1,9 @@
 import pysnow
 import shelve
 import os.path
-
-
 import pytz
-
 from datetime import datetime
 
-
-#Donot need that crap either
-#import time
-#time.gmtime()
-#current_time = time.localtime()
-#utc = '{}{:0>2}{:0>2}'.format('-' if time.altzone > 0 else '+', abs(time.altzone) // 3600, abs(time.altzone // 60) % 60)
-#print(utc)
-#print(type(utc))
-#print(int(utc))
-#print(int('-0500'))
-
-#It looks like we are not going to need it
-#offset =int()
-## Setting the timeModifier
-#if utc!='-0500':
-#	# Need to calculate time offset
-#	print 'Triggered 1'
-#	offset = -int(utc) + int('-0500')
-#	print('Offset is:')
-#	print(offset)
-##else:
-	# No need to do shit
-#	print 'Triggered 2'
 
 osPath = os.path.abspath(__file__)
 filePath = osPath.rsplit('/',1)[0] + '/accountInfo'
@@ -38,16 +12,16 @@ hostName = ()
 userName = ()
 password = ()
 if os.path.isfile(filePath):
-# reading data from existing file
+# Reading data from existing file
 	f=open(filePath)
 	lines=f.readlines()
 	hostName = lines[0].rstrip()
 	userName = lines[1].rstrip()
 	password = lines[2].rstrip()
-
+	print 'Settings successfully loaded from configuration file'
     
 else:
-	# Reading account data to save on disk
+	# Reading input account data to write on disk
 	print 'Please specify the host name you want to operate on'
 	hostName = raw_input()
 	with open (filePath, 'a') as f: f.write(hostName + '\n')
@@ -58,11 +32,9 @@ else:
 	password = raw_input()
 	with open (filePath, 'a') as f: f.write(password)
 	# Account info is stored on the disk now
-
+	print 'Configuration saved'
 # Here is what we got so far
-print(hostName)
-print(userName)
-print(password)
+
 
 # Create client object
 c = pysnow.Client(host=hostName, user=userName, password=password)
@@ -70,54 +42,26 @@ c = pysnow.Client(host=hostName, user=userName, password=password)
 # Define a resource, here we'll use the sctask table API
 sctask = c.resource(api_path='/table/sc_task')
 changeRQ = c.resource(api_path='/table/change_request')
-# CHG0055854
 
-print 'Waiting for the reference ticket number'
+print 'Waiting for a reference ticket number'
 chgNumber = raw_input()
-print 'Ticket number received, ' + chgNumber
+print 'Ticket number received: ' + chgNumber
+print 'Working now...'
 
+# Quering for CHG and CHG's details
 responseCHG = changeRQ.get(query={'number': chgNumber})
-
 responseCHGDict = (responseCHG.one())
-print responseCHGDict['start_date']
-print responseCHGDict['end_date']
-# Response received
-print(type(responseCHGDict['end_date']))
-print(str(responseCHGDict['end_date']))
 
-
-
-
-
+# Working out the time differences, server is running on UTC
 est = pytz.timezone('America/New_York')
 utc = pytz.utc
-# 2018-07-01 14:00:00
-# fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 fmt = '%Y-%m-%d %H:%M:%S'
-
-#winter = datetime(2016, 1, 24, 18, 0, 0, tzinfo=utc)
-#summer = datetime(2016, 7, 24, 18, 0, 0, tzinfo=utc)
-
-#print winter.strftime(fmt)
-#print summer.strftime(fmt)
-
-#print winter.astimezone(est).strftime(fmt)
-#print summer.astimezone(est).strftime(fmt)
-
-# 2018-07-01 14:00:00
 
 startTimeEDT = datetime.strptime(responseCHGDict['start_date'], '%Y-%m-%d %H:%M:%S')
 endTimeEDT = datetime.strptime(responseCHGDict['end_date'], '%Y-%m-%d %H:%M:%S')
 
 startTimeEDT = startTimeEDT.replace(tzinfo=utc)
 endTimeEDT = endTimeEDT.replace(tzinfo=utc)
-print '--------------'
-print'Finals'
-
-#Don't need this one
-#print startTimeDT.strftime(fmt)
-
-#print startTimeEDT.astimezone(est).strftime(fmt)
 
 startTimeEDTs = str(startTimeEDT.astimezone(est).strftime(fmt))
 endTimeEDTs = str(endTimeEDT.astimezone(est).strftime(fmt))
@@ -134,10 +78,8 @@ new_record = {
     'assignment_group': 'System Administration Tier 1'
 }
 
-# Create a new sctask record
+# Creating a new sctask record
 resultSR = sctask.create(payload=new_record)
 
 # Printing result
-print 'Here is your ticket:'
-print resultSR['number']
-#print (responseCHG.one())
+print 'Pre-clone checklist available at: ' + resultSR['number']
